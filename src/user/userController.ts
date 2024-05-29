9298c2d348e2b6cda34001c6eab7539c10029da4
Abhishek <Abhishekmishra@gmail.com>
2024-05-29 12:10:21 +0530

    add create user data into database and response that data

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 export const createUser = async (
   req: Request,
@@ -14,6 +15,23 @@ export const createUser = async (
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
+  // database call
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "user already exist");
+    return next(error);
+  }
+  // create user
+  const newUser = new userModel({
+    name,
+    email,
+    password,
+  });
+  await newUser.save();
 
-  res.json({ message: "successfull" });
+  //   response
+  res.status(201).json({
+    message: "user created successfully",
+    data: newUser,
+  });
 };
---
+++ src/user/userModel.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<User>("User", userSchema);
