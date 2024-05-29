42bd8bf4e0931e3e2a937d83d3f85108c0b3acf6
Abhishek <Abhishekmishra@gmail.com>
2024-05-29 11:53:50 +0530

    add user register validtion

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import globalErrorHandler from "./middlewares/GlobalErrorHandler";
 import userRouter from "./user/userRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "hello jee swagat hai aap sabhi ka" });
