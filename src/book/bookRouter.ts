652c9c0c3eca97966bcbe5bf049f3afdec74a058
Abhishek <Abhishekmishra@gmail.com>
2024-06-01 12:33:39 +0530

    add jwt verification

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -3,6 +3,7 @@ const bookRouter = express.Router();
 import { createBook } from "./bookController";
 import multer from "multer";
 import path from "node:path";
+import authenticate from "../middlewares/authenticate";
 
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/upload"),
@@ -12,6 +13,7 @@ const upload = multer({
 // api routes
 bookRouter.post(
   "/",
+  authenticate,
   upload.fields([
     { name: "coverImage", maxCount: 1 },
     { name: "file", maxCount: 1 },
---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import { verify, JwtPayload } from "jsonwebtoken";
+import { config } from "../config/config";
+// import { AuthRequest } from "./authenticate";
+
+export interface AuthRequest extends Request {
+  userId: string;
+}
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return next(createHttpError(403, "Token is required"));
+  }
+
+  const parsedToken = token.split(" ")[1];
+
+  try {
+    const payload = verify(
+      parsedToken,
+      config.jwtSecret as string
+    ) as JwtPayload;
+    console.log(payload);
+
+    // If you want to attach the payload to the request object
+    const _req = req as AuthRequest;
+    _req.userId = payload.sub as string;
+
+    next();
+  } catch (error) {
+    return next(createHttpError(403, "Invalid token"));
+  }
+};
+
+export default authenticate;
